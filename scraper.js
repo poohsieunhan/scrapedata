a28180ccf61dadec56c2b635a62e9073a54b19a9
linhdd <linhdd85@gmail.com>
2024-06-06 22:09:08 +0700

    done scraper

--- scraper.js
+++ scraper.js
@@ -4,6 +4,18 @@ const scrapeCategory =  (browser,url)=> new Promise(async(resolve,reject)=>{
         await page.goto(url);
         await page.waitForSelector('#webpage')
 
+        const dataCategory = await page.$$eval('#navbar-menu > ul >li',result =>{
+            dataCategory = result.map(el =>{
+                return {
+                    category : el.querySelector('a').innerText,
+                    link: el.querySelector('a').href
+                }
+            })
+            return dataCategory;
+        })
+
+        console.log(dataCategory);
+        await page.close();
         resolve();
     } catch (error) {
         console.log("Loi scrape category: " + error);
